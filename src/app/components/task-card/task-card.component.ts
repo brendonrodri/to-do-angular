7809201c06b274aa4be31f5e1510111398710583
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2024-01-22 00:38:42 -0300

    feat(create-task-card): add input on component

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import Task from 'src/app/services/provider-task/models/task.model';
 
 @Component({
   selector: 'app-task-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.css']
 })
 export class TaskCardComponent implements OnInit {
-
+  @Input() task!: Task;
   constructor() { }
 
   ngOnInit(): void {
